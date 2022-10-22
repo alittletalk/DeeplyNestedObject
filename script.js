a5aa7a39dc359497cc34d14b5e784020658c98d2
xiaoxuhan <xiaoxuhan0426@gmail.com>
2022-10-22 15:11:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function ShowAndHide() {
+    var x = document.getElementById('yuta');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHide1() {
+    var x = document.getElementById('shibaa');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ShowAndHide2() {
+    var x = document.getElementById('homee');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ShowAndHide3() {
+    var x = document.getElementById('broo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ShowAndHide4() {
+    var x = document.getElementById('agee');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ShowAndHide5() {
+    var x = document.getElementById('whoo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
